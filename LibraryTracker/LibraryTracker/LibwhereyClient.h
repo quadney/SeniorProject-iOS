1b81fc0fb38919fa8dfe665f536707b8a56aad5c
ssyyddnneeyy <sydneyrichardson3@gmail.com>
2015-04-02 13:15:43 -0400

    added LibwhereyClient implementation to hopefully abstract the networking from the application

--- LibraryTracker/LibraryTracker/LibwhereyClient.h
+++ LibraryTracker/LibraryTracker/LibwhereyClient.h
@@ -8,16 +8,60 @@
 
 #import <Foundation/Foundation.h>
 
+typedef void (^UniversitiesRequestCompletionBlock)(BOOL success, NSError **error, NSArray *universities);
+typedef void (^RegionsRequestCompletionBlock)(BOOL success, NSError **error, NSArray *regions);
+typedef void (^UpdateUserLocationCompletionBlock)(BOOL success, NSError **error);
+
 @interface LibwhereyClient : NSObject
 
 // talks to the network/API and parses JSON
 
 @property (nonatomic, strong) NSURLSession *session;
-@property (strong, nonatomic) NSURLSessionDataTask *dataTask;
+@property (nonatomic, strong) NSURLSessionDataTask *dataTask;
+
+#pragma mark - LibwhereyClient singleton
 
 + (id)sharedClient;
 
-- (NSMutableArray *)retrieveUniversities;
-- (NSMutableArray *)retrieveRegionsForUniversityWithId:(int)universityId;
+#pragma mark - LibwhereyClient API
+
+/*
+ Returns all Universities in the database
+ 
+ @param completionBlock: A completion block with a NSArray of the Universities
+ */
+- (void)getUniversitiesWithCompletion:(UniversitiesRequestCompletionBlock)completionBlock;
+
+/*
+ Returns all Regions associated with the University
+ 
+ @param completionBlock: A completion block with a NSArray of the regions
+ */
+- (void)getRegionsFromUniversityWithId:(int)universityId completion:(RegionsRequestCompletionBlock)completionBlock;
+
+/*
+ Returns the Region with the specified id
+ 
+ @param completionBlock: A completion block with a NSArray of the regions
+ */
+- (void)getRegionWithId:(int)regionId completion:(RegionsRequestCompletionBlock)completionBlock;
+
+/*
+ Tells the database that a user has entered the region and is studying there
+ 
+ Returns the updated Region
+ 
+ @param completionBlock: A completion block with a NSArray of the regions
+ */
+- (void)userEntersRegionWithId:(int)regionId completion:(UpdateUserLocationCompletionBlock)completionBlock;
+
+/*
+ Tells the database that a user has exited the region
+ 
+ Returns the updated Region
+ 
+ @param completionBlock: A completion block with a NSArray of the regions
+ */
+- (void)userExitsRegionWithId:(int)regionId completion:(UpdateUserLocationCompletionBlock)completionBlock;
 
 @end
--- LibraryTracker/LibraryTracker/LibwhereyClient.m
+++ LibraryTracker/LibraryTracker/LibwhereyClient.m
@@ -7,6 +7,9 @@
 //
 
 #import "LibwhereyClient.h"
+#import "Region.h"
+#import "University.h"
+#import <CoreLocation/CoreLocation.h>
 
 @implementation LibwhereyClient
 
@@ -34,13 +37,136 @@
     return _session;
 }
 
+- (void)retrieveDataFromURL:(NSString *)urlString withCompletionBlock:(void (^)(void))completionBlock {
+    NSURL *url = [NSURL URLWithString:urlString];
+    
+    if (self.dataTask) {
+        // dataTask is already alive, need to stop it
+        [self.dataTask cancel];
+    }
+    
+    self.dataTask = [self.session dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error) {
+            if (error.code != -999) {
+                NSLog(@"%@", error);
+            }
+        }
+        else {
+            NSDictionary *result = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+            // want this to go to the appropriate JSON parser, which can turn this data into the correct classes which correlate to this project
+        }
+    }];
+    
+    if (self.dataTask) {
+        [self.dataTask resume];
+    }
+}
 
-- (NSMutableArray *)retrieveUniversities {
+#pragma mark - LibwhereyClient API implementation
+- (void)getUniversitiesWithCompletion:(UniversitiesRequestCompletionBlock)completionBlock {
+    NSURL *url = [NSURL URLWithString:[self getUniversities]];
+    
+    if (self.dataTask) {
+        // dataTask is already alive, need to stop it
+        [self.dataTask cancel];
+    }
+    
+    self.dataTask = [self.session dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error) {
+            if (error.code != -999) {
+                NSLog(@"%@", error);
+            }
+        }
+        else {
+            NSArray *results = [self universityWithJSONData:data];
+            
+            if (completionBlock) {
+                completionBlock(YES, nil, results);
+            }
+            
+            else {
+                if (completionBlock) {
+                    completionBlock(NO, nil, nil);
+                }
+            }
+        }
+    }];
+    
+    if (self.dataTask) {
+        [self.dataTask resume];
+    }
+}
+
+- (void)getRegionsFromUniversityWithId:(int)universityId completion:(RegionsRequestCompletionBlock)completionBlock {
     
 }
 
-- (NSMutableArray *)retrieveRegionsForUniversityWithId:(int)universityId {
+- (void)getRegionWithId:(int)regionId completion:(RegionsRequestCompletionBlock)completionBlock {
     
+}
+
+- (void)userEntersRegionWithId:(int)regionId completion:(UpdateUserLocationCompletionBlock)completionBlock {
+    
+}
+
+- (void)userExitsRegionWithId:(int)regionId completion:(UpdateUserLocationCompletionBlock)completionBlock {
+    
+}
+
+#pragma mark - URL Creation methods for the api url string
+
+// TODO move to more appropriate class when I figure out how
+
+- (NSString *)getUniversities {
+    return @"http://libwherey.herokuapp.com/api/v1/universities";
+}
+
+- (NSString *)getRegionwithId:(int)regionId {
+    return [NSString stringWithFormat:@"http://libwherey.herokuapp.com/api/v1/regions/%i", regionId];
+}
+
+- (NSString *)getRegionsWithUniversityId:(int)univId {
+    return [NSString stringWithFormat:@"%@/%i/regions", [self getUniversities], univId];
+}
+
+- (NSString *)enterRegionURLWithRegionId:(int)regionId {
+    return [NSString stringWithFormat:@"%@/user_entered_region", [self getRegionwithId:regionId]];
+}
+
+- (NSString *)exitRegionURLWithRegionId:(int)regionId {
+    return [NSString stringWithFormat:@"%@/user_exited_region", [self getRegionwithId:regionId]];
+}
+
+#pragma mark - Parsers - these also need to move when I know how to move them, sigh
+
+- (NSArray *)regionWithJSONData:(id)jsonData {
+    // TODO
+    // the result of this should look like this:
+    //
+    NSMutableArray *regions = [[NSMutableArray alloc] init];
+//    for (NSDictionary *obj in jsonData) {
+////        [regions addObject:[[Region alloc] initWithIdentifier:[obj objectForKey:@"identifier"]
+////                                                       center:[[CLLocation alloc] initWithLatitude:[obj objectForKey:@"latitiude"]
+////                                                                                         longitude:[obj objectForKey:@"longitude"]]
+////                                                       radius:50.0
+////                                                     idNumber:(int)[obj objectForKey:@"id"]]];
+//    }
+    
+    return [NSArray arrayWithArray:regions];
+}
+
+- (NSArray *)universityWithJSONData:(id)jsonData {
+    // the result of this should look like this:
+    // [{"id":1,"name":"University of Florida","created_at":"2015-03-16T22:06:43.359Z","updated_at":"2015-03-16T22:06:43.359Z"},{"id":2,"name":"Florida State","created_at":"2015-04-02T04:18:02.290Z","updated_at":"2015-04-02T04:18:02.290Z"}]
+    NSMutableArray *universities = [[NSMutableArray alloc] init];
+    for (NSDictionary *obj in jsonData) {
+        [universities addObject:[[University alloc] initWithName:[obj objectForKey:@"name"]
+                                                         regions:nil
+                                                        idNumber:(int)[obj objectForKey:@"id"]
+                                 ]
+         ];
+    }
+    return [NSArray arrayWithArray:universities];
 }
 
 @end
